bb7f28d52ecdac9b56d92de1b446fdd30a903516
sungvzer <garganosalvio@gmail.com>
2021-11-03 01:47:20 +0100

    Controllers: implement basic database controller
    This returns the connection with "default" parameters whenever
    it's asked gently

---
+++ src/controllers/databaseController.ts
@@ -0,0 +1,16 @@
+import * as mysql2 from 'mysql2/promise';
+import { ENVIRONMENT } from '../utils/secrets';
+
+export interface LastInsertId extends mysql2.RowDataPacket {
+    "LAST_INSERT_ID()": number;
+}
+
+export const getDatabaseConnection = async () =>
+    await mysql2.createConnection({
+        host: process.env.MYSQL_HOST,
+        password: process.env.MYSQL_PASSWORD_DEV,
+        port: parseInt(process.env.MYSQL_PORT),
+        database: "aurora",
+        // TODO: This needs to be changed once we reach production
+        user: ENVIRONMENT === 'Development' ? "aurora_dev" : "aurora_dev",
+    });
