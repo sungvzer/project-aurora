f6a555583d08bcc476d37a2012391045cb1d50ff
sungvzer <garganosalvio@gmail.com>
2021-11-03 01:48:01 +0100

    API: implement a POST signup endpoint
    This endpoint will sanitize and normalize the request before actually
    calling the User model.

---
+++ src/controllers/userController.ts
@@ -0,0 +1,68 @@
+import { body, check, validationResult } from 'express-validator';
+import { Request, Response } from 'express';
+import User, { UserDatabaseInsertModel } from '../models/User';
+import CurrencyCode, { isCurrencyCode } from '../models/CurrencyCode';
+import AuroraError from '../models/ErrorModel';
+
+export const postSignup = async (req: Request, res: Response): Promise<void> => {
+    /**
+     * Empty Checks
+     */
+    await check("email", "Email should not be blank").notEmpty().run(req);
+    await check("password", "Password should not be blank").notEmpty().run(req);
+    await check("firstName", "First name should not be blank").notEmpty().run(req);
+    await check("lastName", "Last name should not be blank").notEmpty().run(req);
+    await check("birthday", "Birthday should not be blank").notEmpty().run(req);
+    await check("currency", "Currency code should not be blank").notEmpty().run(req);
+
+    /**
+     * Validity Checks
+     */
+    await check("currency", "Currency code is not valid").custom((input) => {
+        return isCurrencyCode(input);
+    }).run(req);
+    await check("email", "Email is not valid").isEmail().run(req);
+    await check("birthday", "Birthday should be a valid ISO date").isDate({ format: "YYYY-MM-DD" }).run(req);
+
+    /**
+     * Body sanitization
+     */
+    await body("email").normalizeEmail({ gmail_remove_dots: false, all_lowercase: true }).run(req);
+
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json(errors.array());
+        return;
+    }
+
+    const email = req.body.email;
+    const plainTextPassword = req.body.password;
+    const lastName = req.body.lastName;
+    const firstName = req.body.firstName;
+    const middleName = req.body.middleName;
+    const currencyCode = CurrencyCode[req.body.currency];
+    const birthday = req.body.birthday;
+
+
+    const userModel: UserDatabaseInsertModel = {
+        birthday: new Date(Date.parse(birthday)),
+        currencyCode,
+        email,
+        firstName,
+        middleName,
+        lastName,
+        plainTextPassword
+    };
+
+    const errorOrSuccess: AuroraError = await User.create(userModel);
+    if (errorOrSuccess.error) {
+        res.status(400).json({
+            ...errorOrSuccess
+        });
+    }
+
+    res.status(200).json({
+        "message": "User signed up correctly",
+    });
+};
