2fb939663c094630a2015fa07b92cacbd5865676
sungvzer <garganosalvio@gmail.com>
2021-11-04 16:48:16 +0100

    Argon util: add `await` to both functions
    This bug prevented the use of `verifyPassword`. IDK why.
    Don't ask me.
    Works now I guess.

--- src/utils/argon.ts
+++ src/utils/argon.ts
@@ -3,10 +3,9 @@ import argon from 'argon2';
 const argonOptions = { hashLength: 32, memoryCost: 5000, parallelism: 12, timeCost: 50, type: argon.argon2i, saltLength: 32, };
 
 export async function hashPassword(password: string): Promise<string> {
-    return argon.hash(password, argonOptions);
+    return await argon.hash(password, argonOptions);
 }
 
 export async function verifyPassword(password: string, hash: string): Promise<boolean> {
-    return argon.verify(hash, password);
+    return await argon.verify(hash, password);
 }
-
