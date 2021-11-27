7311c31fc8e7cbc9793b49225ea3f8adb38a6428
sungvzer <garganosalvio@gmail.com>
2021-11-28 00:30:26 +0100

    API: Fix URL in default response

--- src/controllers/apiController.ts
+++ src/controllers/apiController.ts
@@ -18,7 +18,7 @@ export const defaultError = (req: Request, res: Response): void => {
     let response = new SingleResourceResponse("error");
     response.addError({
         code: "ERR_NOT_FOUND",
-        detail: "Cannot " + req.method + " " + req.path,
+        detail: "Cannot " + req.method + " " + req.originalUrl,
         title: "Not found",
         status: "404"
     });
