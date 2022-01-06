3d1b6aa361eafd5a585a72c42768019c0ca30e14
sungvzer <garganosalvio@gmail.com>
2022-01-06 11:34:02 +0100

    Utils: improve random string generation

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,8 +1,39 @@
-const alphabet = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const lowerAlphabet = 'abcdefghijklmnopqrstuvwxyz';
+const upperAlphabet = lowerAlphabet.toUpperCase();
+const numbers = '0123456789';
 
-export default (length = 16): string => {
+export interface RandomStringOptions {
+    lowerCase?: boolean;
+    upperCase?: boolean;
+    withNumbers?: boolean;
+    additionalCharacters?: string;
+}
+
+export default (
+    length = 16,
+    options: RandomStringOptions = {
+        lowerCase: true,
+        upperCase: false,
+        withNumbers: true,
+        additionalCharacters: '',
+    },
+): string => {
     let str = '';
     let index: number;
+    let alphabet = '';
+    if (options.lowerCase) {
+        alphabet += lowerAlphabet;
+    }
+    if (options.upperCase) {
+        alphabet += upperAlphabet;
+    }
+    if (options.withNumbers) {
+        alphabet += numbers;
+    }
+    if (options.additionalCharacters) {
+        alphabet += options.additionalCharacters;
+    }
+
     for (let i = 0; i < length; i++) {
         index = Math.random() * alphabet.length;
         index = Math.floor(index);
